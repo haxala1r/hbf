aa1d66bd29a1cc1645e1089de70e68b018b59c97
Emin Arslan <me@emin.engineer>
2026-06-04 20:46:36 +0300

    Added command-line options -c and -o

--- app/Interpret.hs
+++ app/Interpret.hs
@@ -26,38 +26,39 @@ newTape :: Tape
 newTape = Tape [] 0 (infRepeat 0)
 
 
-executeOne :: Instr -> Tape -> IO Tape
+executeOne :: (Char -> IO ()) -> Instr -> Tape -> IO Tape
 
-executeOne NextData (Tape ls c (Cons r rs)) =
+executeOne _ NextData (Tape ls c (Cons r rs)) =
   return $ Tape (c : ls) r rs
-executeOne PrevData t = f t
+
+executeOne _ PrevData t = f t
   where
     f (Tape [] c rs) = return $ Tape [] c rs
     f (Tape (l : ls) c rs) = return $ Tape ls l (Cons c rs)
-executeOne Inc (Tape ls c rs) = return $ Tape ls (c + 1) rs
-executeOne Dec (Tape ls c rs) = return $ Tape ls (c - 1) rs
-executeOne Output (Tape ls c rs) = do
-  putChar $ chr $ fromEnum c
+executeOne _ Inc (Tape ls c rs) = return $ Tape ls (c + 1) rs
+executeOne _ Dec (Tape ls c rs) = return $ Tape ls (c - 1) rs
+executeOne pc Output (Tape ls c rs) = do
+  pc $ chr $ fromEnum c
   return $ Tape ls c rs
-executeOne Input (Tape ls _ rs) = do
+executeOne _ Input (Tape ls _ rs) = do
   c <- getChar
   return $ Tape ls (toEnum $ ord c) rs
 
 -- just like when parsing, the loops are the only
 -- tricky part, but they're not hard
-executeOne (Loop is) (Tape ls c rs) =
+executeOne f (Loop is) (Tape ls c rs) =
   if c == 0 then
     return $ Tape ls c rs -- if zero, just skip
   else
     -- if non-zero, execute the entire body, then
     -- keep executing forever until you see zero.
     do
-      t <- executeAll is (Tape ls c rs)
-      executeOne (Loop is) t
+      t <- executeAll f is (Tape ls c rs)
+      executeOne f (Loop is) t
 
-executeAll :: [Instr] -> Tape -> IO Tape
-executeAll is t =
-  foldM (\t' i -> executeOne i t') t is  
+executeAll :: (Char -> IO ()) -> [Instr] -> Tape -> IO Tape
+executeAll f is t =
+  foldM (\t' i -> executeOne f i t') t is  
 
 
 -- test
--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,12 @@ import BF
 import Interpret
 import System.IO
 import CBackend
+import Options.Applicative
 
 
-exec :: [Instr] -> IO ()
-exec is = do
-  _ <- executeAll is newTape
+exec :: (Char -> IO ()) -> [Instr] -> IO ()
+exec f is = do
+  _ <- executeAll f is newTape
   return ()
 
 cleanStr :: String -> String
@@ -25,12 +26,46 @@ getAll = aux []
         l <- getLine
         aux (acc ++ l)
 
+
+data CmdOpts = CmdOpts { compile :: Bool, outFile :: String }
+
+parser :: Parser CmdOpts
+parser = CmdOpts
+  <$> switch (long "compiler"
+             <> short 'c'
+             <> help "whether to compile to C code instead of interpreting")
+  <*> strOption (long "output"
+                <> short 'o'
+                <> value ""
+                <> metavar "OUT"
+                <> help "write output to OUT instead of stdout")
+
+doMain :: CmdOpts -> IO ()
+doMain opts = do
+  input <- getAll
+  case (parse input, outFile opts) of
+    (Just is, "") ->
+      if (compile opts) then
+        print $ emitC is
+      else
+        exec putChar is
+    (Just is, f) ->
+      withFile f WriteMode
+      (\handle -> do
+          if (compile opts) then
+            hPutStr handle $ show $ emitC is
+          else
+            exec (hPutChar handle) is)
+    (Nothing, _) ->
+      putStrLn "parse error (possibly an unmatched ])"
+
+
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  input <- getAll
-  case parse input of
-    Just is -> 
-      print $ emitC is
-    Nothing ->
-      putStrLn "parse error (possibly an unmatched ])"
+  doMain =<< execParser opts
+    where
+      opts = info (parser <**> helper)
+        (fullDesc
+        <> progDesc "Interpret Brainfuck, or compile BF to C"
+        <> header "Interpreter/Compiler for Brainfuck")
