8af3a168556ba40a2514855f540a1b68aa33c726
Emin Arslan <me@emin.engineer>
2026-07-18 12:28:59 +0300

    Fix dependency version, reorganize flake

--- flake.nix
+++ flake.nix
@@ -8,8 +8,17 @@
         mkShell {
           packages = [ghc cabal-install haskell-language-server hlint];
         };
+      pkg = {haskellPackages, ...}:
+        haskellPackages.developPackage {
+          root = ./.;
+        };
     in {
-      
-      packages.aarch64-darwin.default = lp.aarch64-darwin.callPackage sh {};
+      packages.x86_64-linux.default = lp.x86_64-linux.callPackage pkg {};
+      packages.aarch64-linux.default = lp.aarch64-linux.callPackage pkg {};
+      packages.aarch64-darwin.default = lp.aarch64-darwin.callPackage pkg {};
+
+      devShells.x86_64-linux.default = lp.x86_64-linux.callPackage sh {};
+      devShells.aarch64-linux.default = lp.aarch64-linux.callPackage sh {};
+      devShells.aarch64-darwin.default = lp.aarch64-darwin.callPackage sh {};
     };
 }
